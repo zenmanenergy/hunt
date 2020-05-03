61a48e0bbab6093c8c73510c2afef4238e2ae2f9
Steve Nelson <snelson@zenmanenergy.org>
2020-05-03 03:26:58 -0400

    Added debugging tools
    Added simulator tools for destop version.

--- js/compass.js
+++ js/compass.js
@@ -4,9 +4,9 @@ function Compass(){
   //doesn't allow callback functions to access the parent object.
   var parent = this;
 
-  this.cardinalDirection;
-  this.interCardinalDirection;
-  this.heading;
+  this.cardinalDirection="N";
+  this.interCardinalDirection="N";
+  this.heading=0;
   this.hasPermission=false;
   this.onChange=function(heading, cardinalDirection, interCardinalDirection){
     console.log("[Compass]","compass.onChange event. This method can be overriden using: compass.onChange=function(latitude, longitude, accuracy, timestamp){} ")
@@ -16,6 +16,42 @@ function Compass(){
     console.log("[Compass]","compass.onError event. This method can be overriden using: compass.onError=function(type, message, error){} ");
     console.log(type, message, error);
   };
+  this.setHeading = function(_heading){
+    this.heading=_heading;
+    if ((this.heading >= 315 && this.heading < 360) || (this.heading >= 0 && this.heading < 45)){
+      this.cardinalDirection="N";
+
+    }
+    else if (this.heading >= 45 && this.heading < 135) {
+      this.cardinalDirection="E";
+    }
+    else if (this.heading >= 135 && this.heading < 225){
+      this.cardinalDirection="S";
+    }
+    else if (this.heading >= 225 && this.heading < 315){
+      this.cardinalDirection="W";
+    }
+
+    if ((this.heading >= 337 && this.heading < 360) || (this.heading >= 0 && this.heading < 22)){
+      this.interCardinalDirection="N";
+    } else if ((this.heading >= 22 && this.heading < 67)){
+      this.interCardinalDirection="NE";
+    } else if (this.heading >= 67 && this.heading < 112) {
+      this.cardinalDirection="E";
+    } else if (this.heading >= 112 && this.heading < 157) {
+      this.cardinalDirection="SE";
+    } else if ((this.heading >= 157 && this.heading < 202)){
+      this.interCardinalDirection="S";
+    } else if ((this.heading >= 202 && this.heading < 248)){
+      this.interCardinalDirection="SW";
+    } else if ((this.heading >= 248 && this.heading < 292)){
+      this.interCardinalDirection="W";
+    } else if ((this.heading >= 292 && this.heading < 337)){
+      this.interCardinalDirection="NW";
+    }
+    this.onChange(this.heading, this.cardinalDirection, this.interCardinalDirection);
+
+  }
   this.init = function (){
     try{
       DeviceOrientationEvent.requestPermission()
@@ -31,40 +67,7 @@ function Compass(){
                   // document.getElementById("doTiltFB").innerHTML = Math.round(e.beta);
                   // document.getElementById("doDirection").innerHTML = 360-Math.round(e.alpha);
                   //parent.heading = 360-Math.round(e.alpha);
-                  parent.heading = Math.round(e.webkitCompassHeading);
-                  if ((parent.heading >= 315 && parent.heading < 360) || (parent.heading >= 0 && parent.heading < 45)){
-                    parent.cardinalDirection="N";
-
-                  }
-                  else if (parent.heading >= 45 && parent.heading < 135) {
-                    parent.cardinalDirection="E";
-                  }
-                  else if (parent.heading >= 135 && parent.heading < 225){
-                    parent.cardinalDirection="S";
-                  }
-                  else if (parent.heading >= 225 && parent.heading < 315){
-                    parent.cardinalDirection="W";
-                  }
-
-                  if ((parent.heading >= 337 && parent.heading < 360) || (parent.heading >= 0 && parent.heading < 22)){
-                    parent.interCardinalDirection="N";
-                  } else if ((parent.heading >= 22 && parent.heading < 67)){
-                    parent.interCardinalDirection="NE";
-                  } else if (parent.heading >= 67 && parent.heading < 112) {
-                    parent.cardinalDirection="E";
-                  } else if (parent.heading >= 112 && parent.heading < 157) {
-                    parent.cardinalDirection="SE";
-                  } else if ((parent.heading >= 157 && parent.heading < 202)){
-                    parent.interCardinalDirection="S";
-                  } else if ((parent.heading >= 202 && parent.heading < 248)){
-                    parent.interCardinalDirection="SW";
-                  } else if ((parent.heading >= 248 && parent.heading < 292)){
-                    parent.interCardinalDirection="W";
-                  } else if ((parent.heading >= 292 && parent.heading < 337)){
-                    parent.interCardinalDirection="NW";
-                  }
-
-                  parent.onChange(parent.heading, parent.cardinalDirection, parent.interCardinalDirection);
+                  parent.setHeading(Math.round(e.webkitCompassHeading));
 
               })
           } else{
